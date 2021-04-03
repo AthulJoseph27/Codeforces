ddd4702f17289f1ba09d918ae99950459c909b78
AthulJoseph27 <athuljoseph27052001@gmail.com>
2021-04-04 04:29:06 +0530

    Initial Commit

---
+++ 313A.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define push push_back
+#define all(v) v.begin(), v.end()
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(int cc)
+{
+    int n;
+
+    cin >> n;
+
+    if (n >= 0)
+    {
+        cout << n << endl;
+        return;
+    }
+
+    string s1 = to_string(abs(n)), s2 = to_string(abs(n));
+    if (s1.size() == 1)
+    {
+        cout << 0 << endl;
+        return;
+    }
+    s1.erase(s1.end() - 1);
+    s2.erase(s2.end() - 2);
+
+    int n1 = stoi(s1), n2 = stoi(s2);
+    if (n1 < n2)
+    {
+        cout << n1 * -1 << endl;
+    }
+    else
+    {
+        cout << n2 * -1 << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve(1);
+
+    return 0;
+}
