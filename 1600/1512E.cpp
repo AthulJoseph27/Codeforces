c4ecfdbcc3bf4d93f47be74248c1900d11a7afc5
AthulJoseph27 <athuljoseph27052001@gmail.com>
2021-06-01 19:52:25 +0530

    Added 1512E.cpp

--- 1600/1512E.cpp
+++ 1600/1512E.cpp
@@ -65,6 +65,82 @@ void _print(T t, V... v)
 
 void solve(int cc)
 {
+    int n, l, r, s;
+
+    cin >> n >> l >> r >> s;
+
+    int k = r - l + 1;
+
+    // get s with no coins
+
+    /*
+        k = r - l + 1
+
+        low = k(k+1)/2
+        high = n + n-1 + ..n-k+1 => kn - (k(k-1))/2 = k(2n-k+1)/2
+
+        any sum between low and high can be achieved
+        
+    */
+
+    int low = (k * (k + 1)) / 2;
+    int high = (k * (2 * n - k + 1)) / 2;
+
+    if (s < low || s > high)
+    {
+        cout << -1 << '\n';
+        return;
+    }
+
+    vector<int> a;
+    map<int, int> mp;
+
+    for (int i = n; i > 0; i--)
+    {
+        int nk = k - 1;
+        int nlw = (nk * (nk + 1)) / 2;
+        int nhg = (nk * (2 * (i - 1) - nk + 1)) / 2;
+        int ns = s - i;
+        if (ns < nlw || ns > nhg)
+        {
+            continue;
+        }
+        k--;
+        s -= i;
+        a.push_back(i);
+        mp[i]++;
+    }
+
+    assert(s == 0);
+
+    int count = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (count == (l - 1))
+            break;
+        if (mp[i] == 0)
+        {
+            cout << i << ' ';
+            mp[i]++;
+            count++;
+        }
+    }
+
+    for (int i : a)
+        cout << i << ' ';
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (mp[i] == 0)
+        {
+            cout << i << ' ';
+        }
+    }
+
+    // debug(a);
+
+    cout << '\n';
 }
 
 int main()
