d38d075e8e2ffa1d790e70f14dd585baae584607
AthulJoseph27 <athuljoseph27052001@gmail.com>
2021-07-15 22:09:43 +0530

    Added Edu 111 C.cpp

---
+++ Contests/E111/b.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define all(v) v.begin(), v.end()
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(int cc)
+{
+    long long n, a, b;
+    cin >> n >> a >> b;
+    string s;
+    cin >> s;
+
+    if (b >= 0)
+    {
+        cout << ((a + b) * n) << '\n';
+        return;
+    }
+
+    int zero_chunks = 0, one_chunks = 0;
+    s += 'x';
+    for (int i = 1; i <= n; i++)
+        if (s[i - 1] == '0' && s[i] != s[i - 1])
+            zero_chunks++;
+
+    for (int i = 1; i <= n; i++)
+        if (s[i - 1] == '1' && s[i] != s[i - 1])
+            one_chunks++;
+
+    int mx = max(zero_chunks, one_chunks), mn = min(zero_chunks, one_chunks);
+
+    int ans = a * n + (mn + 1) * b;
+
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        solve(i + 1);
+    }
+
+    return 0;
+}
