229de542585bea1192e994e382b05622bd5cf213
AthulJoseph27 <athuljoseph27052001@gmail.com>
2021-08-01 19:46:20 +0530

    Added -1486B.cpp

---
+++ 1500/1486B.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define all(v) v.begin(), v.end()
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(int cc)
+{
+    int n;
+    cin >> n;
+
+    vector<int> Xs(n);
+    vector<int> Ys(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> Xs[i] >> Ys[i];
+    }
+
+    sort(all(Xs));
+    sort(all(Ys));
+
+    /*
+        sum of all x = smX
+        sum of all y = smY
+
+        if(smX%n == 0) -> 1 point, tht is the mid point
+        else -> no of points between the median
+
+        similary for Y
+        
+    */
+
+    long long x, y;
+
+    if (n % 2 != 0)
+    {
+        cout << "1\n";
+        return;
+    }
+
+    x = Xs[n / 2] - Xs[n / 2 - 1] + 1;
+    y = Ys[n / 2] - Ys[n / 2 - 1] + 1;
+
+    cout << (x * y) << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        solve(i + 1);
+    }
+
+    return 0;
+}
