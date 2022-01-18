1e6e2648d9b84bdcd1f64e89318c8772a4a02001
AthulJoseph27 <athuljoseph27052001@gmail.com>
2022-01-18 18:49:47 +0530

    Added 1337

---
+++ 900/1354A.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pi;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(int cc)
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if (b >= a)
+    {
+        cout << b << '\n';
+        return;
+    }
+
+    if (d >= c)
+    {
+        cout << -1 << '\n';
+        return;
+    }
+
+    /*
+        b + c + c + c - d - d - d - d >= a
+
+        b+x(c-d) >= a
+       
+        x(c-d) >= a-b
+
+        if((a-b)%(c-d) == 0){
+            x = (a-b)/(c-d)
+        }else{
+            x = (a-b)/(c-d)+1
+        }
+
+    */
+
+    ll x = (a - b) / (c - d);
+
+    if ((a - b) % (c - d) != 0)
+    {
+        x++;
+    }
+
+    cout << (b + x * c) << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        solve(i + 1);
+    }
+
+    return 0;
+}
