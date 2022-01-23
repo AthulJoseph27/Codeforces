876892ac56aef04cd5fedd7488fd63400816c99a
AthulJoseph27 <athuljoseph27052001@gmail.com>
2022-01-23 21:54:56 +0530

    Added 1000s

---
+++ 1000/1450B.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pi;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(int cc)
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<pi> a(n);
+
+    for (auto &i : a)
+        cin >> i.first >> i.second;
+
+    for (int i = 0; i < n; i++)
+    {
+        // move to this point
+        vector<pi> tmp = a;
+
+        sort(all(a), [&](const pi aa, const pi bb)
+             { return (abs(aa.first - tmp[i].first) + abs(aa.second - tmp[i].second)) <
+                      (abs(bb.first - tmp[i].first) + abs(bb.second - tmp[i].second)); });
+
+        int dist = abs(a.back().first - tmp[i].first) + abs(a.back().second - tmp[i].second);
+
+        if (dist <= k)
+        {
+            cout << 1 << '\n';
+            return;
+        }
+
+        a = tmp;
+    }
+
+    cout << -1 << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        solve(i + 1);
+    }
+
+    return 0;
+}
---
+++ 1000/1469B.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pi;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(int cc)
+{
+    int n;
+    cin >> n;
+
+    vi r(n);
+
+    for (int &i : r)
+        cin >> i;
+
+    int m;
+    cin >> m;
+    vi b(m);
+
+    for (int &i : b)
+        cin >> i;
+
+    int mx = 0;
+
+    vi pref_r = {0}, pref_b = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        pref_r.push_back(pref_r.back() + r[i]);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        pref_b.push_back(pref_b.back() + b[i]);
+    }
+
+    int mpr = *max_element(all(pref_r)), mpb = *max_element(all(pref_b));
+
+    cout << max(0, mpr + mpb) << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        solve(i + 1);
+    }
+
+    return 0;
+}
