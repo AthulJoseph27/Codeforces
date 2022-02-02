3b7dc648c284d25031365bd644036582892effdb
AthulJoseph27 <athuljoseph27052001@gmail.com>
2022-02-02 19:40:09 +0530

    Added 1200s

---
+++ 1200/1581B.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pi;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(int cc)
+{
+    ll n, m, k;
+
+    cin >> n >> m >> k;
+
+    // 1 2 3 4
+
+    // n-1 -> dia = n
+    // n-2 -> n-1
+    // dia = n - (m-(n-1)) => n-m+n+1 =2n-m+1
+
+    ll mx_edges = (n * (n - 1)) / 2;
+
+    if (m < (n - 1))
+    {
+        cout << "NO\n";
+        return;
+    }
+
+    if (m > mx_edges)
+    {
+        cout << "NO\n";
+        return;
+    }
+
+    if (n == 1)
+    {
+        if (k > 1)
+        {
+            cout << "YES\n";
+            return;
+        }
+        cout << "NO\n";
+        return;
+    }
+
+    if (m == mx_edges)
+    {
+        if (k > 2)
+        {
+            cout << "YES\n";
+            return;
+        }
+        cout << "NO\n";
+        return;
+    }
+
+    if (k > 3)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        solve(i + 1);
+    }
+
+    return 0;
+}
