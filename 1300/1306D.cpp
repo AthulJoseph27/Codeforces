909e8f8241d0b96fc5fb9bef82d3acb72b5f99a1
AthulJoseph27 <athuljoseph27052001@gmail.com>
2022-02-14 18:29:08 +0530

    Added 1300s

---
+++ 1300/1306D.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pi;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(int cc)
+{
+    int n;
+    cin >> n;
+
+    int k;
+    cin >> k;
+
+    if (n <= k)
+    {
+        cout << "1\n";
+        return;
+    }
+
+    set<int> factors = {1};
+    int lm = sqrt(n) + 5;
+    for (int i = 2; i <= lm; i++)
+    {
+        if (n % i == 0)
+        {
+            factors.insert(i);
+            factors.insert(n / i);
+        }
+    }
+
+    int last = -1;
+
+    for (int i : factors)
+    {
+        if (i <= k)
+            last = i;
+    }
+
+    int x = n / last;
+
+    if (n % last != 0)
+    {
+        x = n;
+    }
+
+    cout << x << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        solve(i + 1);
+    }
+
+    return 0;
+}
